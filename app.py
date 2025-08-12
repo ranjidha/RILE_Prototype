48ca55de09a279b867643e6e8b32e61c37725967
Dr. Red <46902933+ranjidha@users.noreply.github.com>
2025-08-12 13:04:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,7 @@ elif page == "🌐 Graph":
     st.title("Collaboration Network")
     st.info("Generating the graph. This may take a few seconds if the dataset is large...")
     html_graph = display_graph(users, connections)
+    st.set_page_config(layout="wide")
     st.components.v1.html(html_graph, height=1000, width=None, scrolling=True)
 
 
